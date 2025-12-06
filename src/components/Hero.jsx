5b6107a406fe3eeedf04e3f7b69ffa4b3c852b7c
Shukrael <shukraelmohammed32@gmail.com>
2025-12-06 13:27:56 +0300

    change color to red in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => (
         Payment Method.
       </h1>
 
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5 text-green-500`}>
         Our team of experts uses a methodology to identify the credit cards most
         likely to fit your needs. We examine annual percentage rates, annual
         fees.
